76f1f803a96c4dc5d0e6536f57ec2f74a0c44ae3
peshawa71 <peshawaomer509@gmail.com>
2025-08-09 17:52:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from moviepy import VideoFileClip, ImageClip, concatenate_videoclips, CompositeVideoClip
+from moviepy import *
 import os
 from dotenv import load_dotenv
 from telethon.sync import TelegramClient
